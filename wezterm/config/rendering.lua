101a040839da17e0798d5635cf371405ae6edda7
qxuken <>
2025-11-20 14:56:30 +0000

    Set fps to 240
    FossilOrigin-Name: e1947341d622cb161597049f2c7d17c25665ec89e7114458ec58c2614eed1e11

--- wezterm/config/rendering.lua
+++ wezterm/config/rendering.lua
@@ -4,13 +4,13 @@ local M = {}
 
 -- conforming to https://github.com/wez/wezterm/commit/e4ae8a844d8feaa43e1de34c5cc8b4f07ce525dd
 M.apply_to_config = function(c)
-	if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-		c.front_end = "WebGpu"
-		c.webgpu_power_preference = "HighPerformance"
-	end
+  if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    c.front_end = "WebGpu"
+    c.webgpu_power_preference = "HighPerformance"
+  end
 
-	c.max_fps = 144
-	c.animation_fps = 144
+  c.max_fps = 240
+  c.animation_fps = 240
 end
 
 return M
